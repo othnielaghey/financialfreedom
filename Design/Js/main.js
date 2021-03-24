80ff1d4924011df9f72d60098780089770e7ede6
TheBlackPharaon <59323204+TheBlackPharaon@users.noreply.github.com>
2021-03-24 18:46:33 +0000

    master

---
+++ Design/Js/main.js
@@ -0,0 +1,11 @@
+//document ready
+$(function () {
+
+    //if mobile burger menu clicked
+    $('#toggle').click(function () {
+        $(this).toggleClass('active');
+        $('#overlay').toggleClass('open');
+    });
+
+
+});
